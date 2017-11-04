028163db8723e75a7b13187944f9eb619ee0d8cb
Glitch (wry-tower) <none>
2017-11-04 00:31:43 +0000

    Checkpoint

--- server.js
+++ server.js
@@ -23,23 +23,6 @@ app.get("/whoami", function (request, response) {
   response.json({"ipaddress": methods.getClientIp(request), "language": methods.getLanguage(request), "software": methods.getSoftware(request)});
 });
 
-app.get("/dreams", function (request, response) {
-  response.send(dreams);
-});
-
-// could also use the POST body instead of query string: http://expressjs.com/en/api.html#req.body
-app.post("/dreams", function (request, response) {
-  dreams.push(request.query.dream);
-  response.sendStatus(200);
-});
-
-// Simple in-memory store for now
-var dreams = [
-  "Find and count some sheep",
-  "Climb a really tall mountain",
-  "Wash the dishes"
-];
-
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
